f2840354081d6cb5e580e7a4d682ef02f44fcf13
Muhammad <ranaahmad131@gmail.com>
2023-11-03 00:52:32 +0500

    All good

--- src/Nav.js
+++ src/Nav.js
@@ -1,10 +1,10 @@
 import React from "react";
-
+import logo from "./logo.png";
 function Nav() {
   return (
-    <div className="flex px-2 justify-center">
+    <div className="flex px-2 mx-7 justify-between">
       <div className="flex items-center">
-        <img src="" alt="logo" className="mr-4" />
+        <img src={logo} alt="logo" className="mr-4" />
       </div>
       <nav className="flex justify-center items-center">
         <ul className="navbar-nav cursor-pointer py-2 px-4  text-blue-500 flex space-x-3">
@@ -21,6 +21,9 @@ function Nav() {
             <a href="#">Contact</a>
           </li>
         </ul>
+        <button className="rounded-lg bg-purple-400 text-black-400 font-medium px-3 py-1.5  ">
+          Login
+        </button>
       </nav>
     </div>
   );
